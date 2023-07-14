5988c22022de169df196edf7a25eecbe8016ce09
dep1engenharia <132014184+dep1engenharia@users.noreply.github.com>
2023-07-14 21:33:17 +0100

    Add: First Commit

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from os import walk
 import cv2
+
 from lib.filters import get_grayscale, thresholding, pytesseract
 from lib.format_output import format_output
 
@@ -10,10 +11,27 @@ def apply_filter(plate):
     return thresh
 
 
-def scan_plate(image):
-    custom_config = r'-c tessedit_char_blacklist=abcdefghijklmnopqrstuvwxyz/ --psm 6'
-    plate_number = (pytesseract.image_to_string(image, config=custom_config))
-    return plate_number[:-2]
+# def scan_plate(image):
+ # custom_config = r'-c tessedit_char_blacklist=abcdefghijklmnopqrstuvwxyz/ --psm 6'
+#    plate_number = (pytesseract.image_to_string(image, config=custom_config))
+#    return plate_number[:-1]
+
+def extract_license_plate(input_image):
+    try:
+        blacklisted_chars = 'abcdefghijklmnopqrstuvwxyz/'
+        page_segmentation_mode = 6
+        custom_config = f'-c tessedit_char_blacklist={blacklisted_chars} --psm {page_segmentation_mode}'
+
+        plate_number = pytesseract.image_to_string(input_image, config=custom_config)
+        plate_number = plate_number.strip()
+
+        # Optionally, apply additional post-processing to the plate number string here.
+
+        return plate_number[-8:]
+
+    except Exception as e:
+        print(f"Error extracting license plate: {e}")
+        return None
 
 
 def validate_plate(plate_number, authorized_plate):
@@ -30,7 +48,9 @@ def main():
         '../images/placa1.jpg',
         '../images/placa2.jpg',
         '../images/placa3.jpg',
-        '../images/placa4.jpg'
+        '../images/placa4.jpg',
+        '../images/placa5.png',
+        '../images/placa6.jpg',
     ]
 
     plates = []
@@ -54,7 +74,7 @@ def main():
 
     # Calls the function scan_plate() passing the plate image with filter applied
     for i in range(len(plates_filter_applied)):
-        plates_numbers.append(scan_plate(plates_filter_applied[i]))
+        plates_numbers.append(extract_license_plate(plates_filter_applied[i]))
         data[i].append(plates_numbers[i])
 
     # Calls the function validate_plate() passing the plate number
